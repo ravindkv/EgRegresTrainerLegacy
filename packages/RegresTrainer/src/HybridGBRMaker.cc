f6225ddd8046e78c906bce4362cd9cf5f0d143e8
arun <arun.kumar@cern.ch>
2021-09-29 16:45:17 +0200

    sc reg for HLT up

--- packages/RegresTrainer/src/HybridGBRMaker.cc
+++ packages/RegresTrainer/src/HybridGBRMaker.cc
@@ -321,8 +321,8 @@ void HybridGBRMaker::runEB(const string& cutBase, const string& cutEB, const str
     m_cutEB = cutEB;
     TCut cutCentral(cutBase.c_str());
     //TCut cutCombination(cutComb.c_str());
-    //TCut cutBarrel(cutEB.c_str());
-    TCut cutBarrel("(1)");
+    TCut cutBarrel(cutEB.c_str());
+    //TCut cutBarrel("(1)");
     cout << "INFO: Cuts for EB training = '" << string(cutCentral && cutBarrel) << "'\n";
     //cout << "INFO: Cuts for combination training    = '" << string(cutCombination) << "'\n";
     // weight * cuts
