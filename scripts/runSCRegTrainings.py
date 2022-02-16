cc48ca1eebc2c4513975efb8a733c297a1fc20c5
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2022-02-16 01:27:34 +0100

    Running version with CMSSW_10_6_1

--- scripts/runSCRegTrainings.py
+++ scripts/runSCRegTrainings.py
@@ -8,11 +8,12 @@ except ImportError:
     raise ImportError('module regtools not found, please add "python" to PYTHON27PATH or PYTHONPATH as appropriate eg\n  export PYTHON27PATH=$PYTHON27PATH:python\nand try again')
 import time
 import argparse
-def main():  
+def main():
 
     parser = argparse.ArgumentParser(description='runs the SC regression trainings')
     parser.add_argument('--era',required=True,help='year to produce for, 2016, 2017, 2018 are the options')
-    parser.add_argument('--input_dir','-i',default='/home/hep/wrtabb/Egamma/input_trees/Run3_2021',help='input directory with the ntuples')
+    # parser.add_argument('--input_dir','-i',default='/home/hep/wrtabb/Egamma/input_trees/Run3_2021',help='input directory with the ntuples')
+    parser.add_argument('--input_dir','-i',default='/afs/cern.ch/user/r/rasharma/work/EGamma-POG/HLT_tasks/CPUtoGPUTransition/analyzer/CMSSW_12_0_1/src',help='input directory with the ntuples')
     parser.add_argument('--output_dir','-o',default="results",help='output dir')
     args = parser.parse_args()
 
@@ -20,13 +21,13 @@ def main():
     #step 2, apply the mean to the real IC sample and save the result in a tree
     #step 3, retrain the resolution for the real IC on the corrected energy
     run_step1 = True
-    run_step2 = True
-    run_step3 = True
+    run_step2 = False
+    run_step3 = False
 
     #setup the selection (event number cuts come later)
-    cuts_name = "stdCuts" 
-    base_ele_cuts = "(mc.energy>0 && ssFrac.sigmaIEtaIEta>0 && ssFrac.sigmaIPhiIPhi>0 && {extra_cuts})"
-    
+    cuts_name = "stdCuts"
+    base_ele_cuts = "(eg_gen_energy>0 && eg_sigmaIEtaIEta>0 && {extra_cuts})"
+
     #prefixes all the regressions produced
     if args.era=='2021Run3':
         base_reg_name = "2021Run3"
@@ -34,23 +35,33 @@ def main():
         input_real_ic  = "{}/DoubleElectron_FlatPt-1To500_FlatPU0to70_120X_mcRun3_2021_realistic_v6-v1_AODSIM.root".format(args.input_dir)
         ideal_eventnr_cut = "evt.eventnr%5==0"
         real_eventnr_cut = "evt.eventnr%5==1"
+    elif args.era=='Run3':
+        base_reg_name = "Run3HLT"
+        # input_ideal_ic  = "{}/HLTAnalyzerTree_IDEAL.root".format(args.input_dir)
+        # input_real_ic = "{}/HLTAnalyzerTree_REAL.root".format(args.input_dir)
 
+        input_ideal_ic  = "{}/ideal.root".format(args.input_dir)
+        input_real_ic = "{}/real.root".format(args.input_dir)
+
+        ideal_eventnr_cut = "(1)"  #4million electrons (we determined 4 million was optimal but after the 2017 was done)
+        real_eventnr_cut = "(1)" #4million electrons (we determined 4 million was optimal but after the 2017 was done)
     else:
-        raise ValueError("era {} is invalid, the only available option is 2021Run3".format(era))
+        raise ValueError("era {} is invalid, the only available option is 2021Run3/Run3".format(era))
 
-    
+
     regArgs = RegArgs()
     regArgs.input_training =  str(input_ideal_ic)
     regArgs.input_testing = str(input_ideal_ic)
     regArgs.set_sc_default()
+    regArgs.tree_name = "egHLTRun3Tree"
     regArgs.cfg_dir = "configs"
-    regArgs.out_dir = "results/resultsSC" 
+    regArgs.out_dir = "results/resultsSC"
     regArgs.cuts_name = cuts_name
     regArgs.base_name = "{}_IdealIC_IdealTraining".format(base_reg_name)
     regArgs.cuts_base = base_ele_cuts.format(extra_cuts = ideal_eventnr_cut)
     regArgs.ntrees = 1500
- 
-    print """about to run the supercluster regression with: 
+
+    print """about to run the supercluster regression with:
     name: {name}
     ideal ic input: {ideal_ic}
     real ic input: {real_ic}
@@ -65,7 +76,7 @@ steps to be run:
         os.makedirs(args.output_dir)
 
     if run_step1: regArgs.run_eb_and_ee()
-    
+
     regArgs.do_eb = True
     forest_eb_file = regArgs.output_name()
     regArgs.do_eb = False
@@ -74,7 +85,7 @@ steps to be run:
     regArgs.base_name = "{}_RealIC_IdealTraining".format(base_reg_name)
     input_for_res_training = str(regArgs.applied_name()) #save the output name before we change it
     input_for_input_for_res_training = str(input_real_ic)
-    
+
     # Set scram arch
     arch = "slc7_amd64_gcc700"
     if run_step2: subprocess.Popen(["bin/"+arch+"/RegressionApplierExe",input_for_input_for_res_training,input_for_res_training,"--gbrForestFileEE",forest_ee_file,"--gbrForestFileEB",forest_eb_file,"--nrThreads","4","--treeName",regArgs.tree_name,"--writeFullTree","1","--regOutTag","Ideal"]).communicate()
@@ -82,7 +93,7 @@ steps to be run:
     regArgs.base_name = "{}_RealIC_RealTraining".format(base_reg_name)
     regArgs.input_training = input_for_res_training
     regArgs.input_testing = input_for_res_training
-    regArgs.target = "mc.energy/(sc.rawEnergy*regIdealMean)"
+    regArgs.target = "eg_gen_energy/(eg_rawEnergy*regIdealMean)"
     regArgs.fix_mean = True
     regArgs.cuts_base = base_ele_cuts.format(extra_cuts = real_eventnr_cut)
     if run_step3: regArgs.run_eb_and_ee()
